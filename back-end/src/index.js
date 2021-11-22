2646537bd00ae7a26c3548e0bd537f312eb9ed5c
hdhuong <80466690+hdhuong@users.noreply.github.com>
2021-11-22 21:46:38 +0700

    #update api auth , user

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,10 +1,36 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const port = 8080;
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+
+dotenv.config();
+
+// connect to db
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello xin chao");
 });
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
